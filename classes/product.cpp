33494bfa93c55da4ae6ee53d19368b6437a333f4
Omar Magdy <hammadomar294@gmail.com>
2026-04-20 01:57:30 +0200

    built the product.cpp skeleton

--- classes/product.cpp
+++ classes/product.cpp
@@ -1,4 +1,108 @@
-#include <product.h>
+#include "product.h"
 
 using namespace std;
+
+#pragma region getters and setters
+
+int Product::GetId() const
+{
+    return Id;
+}
+
+void Product::SetId(int id)
+{
+    if (id < 0) return;
+    Id = id;
+}
+
+string Product::GetName() const
+{
+    return Name;
+}
+
+void Product::SetName(const string &name)
+{
+    if (name.empty()) return;
+    Name = name;
+}
+
+double Product::GetPrice() const
+{
+    return Price;
+}
+
+void Product::SetPrice(double price)
+{
+    if (price < 0) return;
+    Price = price;
+}
+
+int Product::GetQuantity() const
+{
+    return Quantity;
+}
+
+void Product::SetQuantity(int quantity)
+{
+    if (quantity < 0) return;
+    Quantity = quantity;
+}
+
+#pragma endregion
+
+// constructor
+Product::Product(int id, const string &name, double price, int quantity)
+{
+    Id = id;
+    Name = name;
+    Price = price;
+    Quantity = quantity;
+}
+
+#pragma region helpers
+
+bool Product::isSufficient(int amount) const
+{
+    return GetQuantity() >= amount;
+}
+
+bool Product::isOutOfStock() const
+{
+    return Quantity == 0;
+}
+
+bool Product::NeedReStock() const
+{
+    return Quantity <= 3;
+}
+
+#pragma endregion
+
+#pragma region bussiness
+
+bool Product::Sell(int amount)
+{
+    if (amount <= 0)
+        return false;
+    if (!isSufficient(amount))
+        return false;
+    Quantity -= amount;
+    return true;
+}
+
+bool Product::ReName(const string &new_name)
+{
+    if (new_name.empty()) return false;
+    Name = new_name;
+    return true;
+}
+
+bool Product::AddStock(int quantity)
+{
+    if (quantity < 0) return false;
+    Quantity += quantity;
+    return true;
+}
+
+#pragma endregion
 
